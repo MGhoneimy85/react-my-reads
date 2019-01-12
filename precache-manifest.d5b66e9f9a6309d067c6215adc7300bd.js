d8d488423939de694fda40bbe04857404cd04fd3
Mohamed El-Ghoneimy <ghoneishy@hotmail.com>
2019-01-12 14:16:13 +0200

    Updates

---
+++ precache-manifest.d5b66e9f9a6309d067c6215adc7300bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "740a0cdd0115a6e877b15a55886ce477",
+    "url": "/react-my-reads/static/media/search.740a0cdd.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/react-my-reads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/react-my-reads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "683e8b35714de30ed5be",
+    "url": "/react-my-reads/static/js/runtime~main.683e8b35.js"
+  },
+  {
+    "revision": "3d5a7691df3d2b5a0d86",
+    "url": "/react-my-reads/static/js/main.3d5a7691.chunk.js"
+  },
+  {
+    "revision": "cfd944a01285ede924b0",
+    "url": "/react-my-reads/static/js/1.cfd944a0.chunk.js"
+  },
+  {
+    "revision": "3d5a7691df3d2b5a0d86",
+    "url": "/react-my-reads/static/css/main.e821e019.chunk.css"
+  },
+  {
+    "revision": "f3d44ae37bc44aa37fd90c2588c35563",
+    "url": "/react-my-reads/index.html"
+  }
+];
